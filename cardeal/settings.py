d1bb231845c32e267d26b2bb873689dc8a77639b
Douglous Sobei <douglousmangoyi@gmail.com>
2023-02-26 16:50:26 +0300

    Team models

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "cardeal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR / "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cardeal_db",
+        "USER": "postgres",
+        "PASSWORD": "sobei",
+        "HOST": "localhost",
     }
 }
 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Teams(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    desgination = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/%Y/%m/%d")
+    facebook_link = models.URLField(max_length=95)
+    twiter_link = models.URLField(max_length=80)
+    created_date = models.DateTimeField(auto_now_add=True)
