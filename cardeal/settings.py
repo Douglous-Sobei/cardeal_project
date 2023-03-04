72950e7f864816a819b764026b9ff5ebf3c67db2
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-04 09:35:04 +0300

    Updated

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,14 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
-STATIC_URL = "/static/"
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'statifiles')
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR / "static")
+    os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR, "media"
 MEDIA_URL = "/media/"
 
 # Default primary key field type
--- pages/admin.py
+++ pages/admin.py
@@ -3,14 +3,4 @@ from .models import Team
 from django.utils.html import format_html
 # Register your models here.
 
-class TeamAdmin(admin.ModelAdmin):
-    def Thumbnail(self, object):
-        return format_html('<img src="{}" width="40" style="border-radius: 50px; />'.format(object.image.url))
-    Thumbnail.short_description = "Image"
-
-    list_display = ("id", "Thumbnail", "first_name", "desgination", "created_date")
-    list_display_links = ("id", "Thumbnail", "first_name")
-    search_fields = ("first_name", "last_name", "desgination")
-    list_filter = ("desgination",)
-
-admin.site.register(Team, TeamAdmin)
+admin.site.register(Team)
