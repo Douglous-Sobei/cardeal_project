7a6a90557e1b7beef745d77aa42e4de281b47e45
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-04 20:48:08 +0300

    Cusomized teams

--- pages/admin.py
+++ pages/admin.py
@@ -3,4 +3,14 @@ from .models import Team
 from django.utils.html import format_html
 # Register your models here.
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.image.url))
+    thumbnail.short_description = "image"
+
+    list_display = ("id", "thumbnail", "first_name", "last_name", "designation", "created_date")
+    list_display_links = ("id", "thumbnail", "first_name")
+    search_fields = ("first_name", "last_name", "designation")
+    list_filter = ("designation",)
+
+admin.site.register(Team, TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Team(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    desgination = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images/%Y/%m/%d")
     facebook_link = models.URLField(max_length=95)
     twitter_link = models.URLField(max_length=80)
