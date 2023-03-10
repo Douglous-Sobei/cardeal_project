3ce290bbf2ade09d7586652526fd9ed20f6a3e73
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-10 04:10:24 +0300

    user login, registration and logout functionality

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts.apps.AccountsConfig",
     "cars.apps.CarsConfig",
     "pages.apps.PagesConfig",
     "django.contrib.admin",
@@ -128,8 +129,16 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+# media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+# messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
