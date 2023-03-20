5a4beef5a79ccbddb664faea180ec80a5f14807d
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-20 07:48:42 +0300

    added allowed hosts

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cardeal.herokuapp.com", "cardealapp.co", "www.cardealapp.co"]
 
 LOGIN_REDIRECT_URL = "dashboard"
 
