e5873fc4878663f109c9440617dce03798e9f0f2
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-22 16:42:53 +0300

    staticfiles storage

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST_PASSWORD = "cblceatctwnntong"
 EMAIL_USE_TLS = True
 
 # Whitenoise settings
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
