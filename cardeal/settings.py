a9f4e9a8be0dca0db4473a93ef4d99f73219d4cb
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-22 13:31:44 +0300

    configured my static files

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -89,17 +89,17 @@ WSGI_APPLICATION = "cardeal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "cardeal_db",
-        "USER": "postgres",
-        "PASSWORD": "SOBEI",
-        "HOST": "localhost",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "cardeal_db",
+#         "USER": "postgres",
+#         "PASSWORD": "SOBEI",
+#         "HOST": "localhost",
+#     }
+# }
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:SOBEI@localhost/cardeal_db')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:SOBEI@localhost/cardeal_db')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cardeal/static')
 ]
