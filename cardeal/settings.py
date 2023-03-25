52da9f126990440266816cec720c3bbeddb54fb7
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-26 02:13:42 +0300

    update

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = config("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ["blooming-hamlet-92814.herokuapp.com"]
+DEBUG = True
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1"
+]
 
 LOGIN_REDIRECT_URL = "dashboard"
 
