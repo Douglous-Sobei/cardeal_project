692f1ac75bcdff4c7069de5ee6509029ff9b6eb2
Douglous Sobei <douglousmangoyi@gmail.com>
2023-08-22 17:01:18 +0300

    google account

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -159,13 +159,14 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# SITE_ID = 1
+SITE_ID = 1
 
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "douglousmangoyi@gmail.com"
-# EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "douglousmangoyi@gmail.com"
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
 
 # Whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
