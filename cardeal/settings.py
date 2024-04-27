193bba53565c492779ed4a6fce828b1cb5f6e2da
Douglous Sobei <douglousmangoyi@gmail.com>
2024-04-27 05:25:29 +0300

    configuration of smtp

--- cardeal/settings.py
+++ cardeal/settings.py
@@ -180,7 +180,7 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 
-AWS_STORAGE_BUCKET_NAME = "proshop-bucket-products"
+AWS_STORAGE_BUCKET_NAME = "mangoyi-images"
 
 
 if os.getcwd() == "/app":
